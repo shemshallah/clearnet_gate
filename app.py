5792ea1e3cd16bb114d46b4632c1a3ec1e5ab7ef
shemshallah <shemshallah@gmail.com>
2025-10-20 13:14:21 -0700

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from datetime import datetime
 import subprocess
 import threading
 
-# Production Logging
+# roduction Logging
 logging.basicConfig(level=logging.WARNING)
 logger = logging.getLogger(__name__)
 
